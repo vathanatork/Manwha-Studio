dc7eeb566f2147e64c50cefd3fa540e262fcd0eb
vathanatork <vath2503@gmail.com>
2026-05-31 22:38:26 +0700

    Add Vercel Analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Analytics } from '@vercel/analytics/react';
 import { Layout } from './components/layout';
 import { HomePage, PricingPage, DocsPage, TermsPage, PrivacyPage, LicensePage } from './pages';
 
@@ -15,6 +16,7 @@ function App() {
           <Route path="/license" element={<LicensePage />} />
         </Routes>
       </Layout>
+      <Analytics />
     </BrowserRouter>
   );
 }
